0631dd30bc25f96b89fd4bb2c94366ca654947c3
Thiago Ventura Barbosa <160201547+devthiagoventura@users.noreply.github.com>
2025-03-12 12:49:39 -0300

    Create 04_initials.py

---
+++ 1-hello-world/04_initials.py
@@ -0,0 +1,9 @@
+# Fato curioso: Só usei python uma vez na tentativa de um projeto de calendario 
+
+print('DDDD   L')
+print('D   D  L')
+print('D   D  L')
+print('D   D  L')
+print('D   D  L')
+print('D   D  L')
+print('DDDD   LLLLL')
